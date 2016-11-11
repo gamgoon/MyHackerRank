86653cb5f0061c1b65815a03683d73a10def1d7f
gamgoon <gamgoon@gmail.com>
2016-11-12 03:16:37 +0900

    Day11, Day15

---
+++ src/main/java/thirtyDays/day15_LinkedList/Node.java
@@ -0,0 +1,10 @@
+package main.java.thirtyDays.day15_LinkedList;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+		data = d;
+		next = null;
+	}
+}
---
+++ src/main/java/thirtyDays/day15_LinkedList/Solution.java
@@ -0,0 +1,39 @@
+package main.java.thirtyDays.day15_LinkedList;
+
+import java.util.Scanner;
+
+class Solution {
+	public static Node insert(Node head, int data) {
+		if(head == null) {
+			return new Node(data);
+		}
+		Node start = head;
+		while(start.next != null) {
+			start = start.next;
+		}
+		
+		start.next = new Node(data);
+		return head;
+	}
+	
+	public static void display(Node head) {
+		Node start = head;
+		while(start != null) {
+			System.out.println(start.data + " ");
+			start = start.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Node head = null;
+		int N = sc.nextInt();
+		
+		while(N-- > 0) {
+			int ele = sc.nextInt();
+			head = insert(head, ele);
+		}
+		display(head);
+		sc.close();
+	}
+}
