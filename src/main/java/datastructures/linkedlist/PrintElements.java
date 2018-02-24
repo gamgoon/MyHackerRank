4434a35c9f58cf9565e6b953843a0036e177b159
gamgoon <gamgoon@gmail.com>
2018-02-24 09:21:27 +0900

    Print the Elements of a Linked list

---
+++ src/main/java/datastructures/linkedlist/PrintElements.java
@@ -0,0 +1,23 @@
+package main.java.datastructures.linkedlist;
+
+/*
+  Print elements of a linked list on console
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+public class PrintElements {
+    static void print(Node head) {
+        if (head == null)
+            return;
+        System.out.println(head.data);
+        print(head.next);
+    }
+}
