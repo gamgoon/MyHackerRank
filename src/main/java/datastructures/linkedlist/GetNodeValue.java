45849053d40b52390cb301aa2264cfa63b5a7a59
gamgoon <gamgoon@gmail.com>
2018-02-27 18:09:45 +0900

    Get Node Value

---
+++ src/main/java/datastructures/linkedlist/GetNodeValue.java
@@ -0,0 +1,19 @@
+package main.java.datastructures.linkedlist;
+
+public class GetNodeValue {
+    public static int getNodeValue(Node head, int n) {
+
+        Node resultNode = head;
+        int index = 0;
+        while (head.next != null) {
+            head = head.next;
+            if (index > n)
+                resultNode = resultNode.next;
+
+            index++;
+        }
+
+        return resultNode.data;
+    }
+
+}
